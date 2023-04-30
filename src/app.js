33e0b32d8fd9e3ff38aa679b1bd7c7bfe7e41916
Rocio Fabrykant <fabrykantr@gmail.com>
2023-04-30 16:15:45 -0300

    estructura

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import  {Server} from 'socket.io';
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
+import viewsRouter from './routes/views.router.js'
 
 const app = express();
 
@@ -10,8 +11,8 @@ app.use(express.static(`${__dirname}/public`));
 app.engine('handlebars',handlebars.engine());
 app.set('views',`${__dirname}/views`);
 app.set('view engine','handlebars');
+app.use('/',viewsRouter);
 
-app.use('/')
 const server = app.listen(8080,()=>{
     console.log('server running');
 })
